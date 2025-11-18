ebb57e485274c66d296c2d869f9ddd0f98b897c6
JeonHaeChan <geojeonhc@gmail.com>
2025-11-18 14:13:16 +0900

    STUDY: 항목 7 다형성을 가진 클래스에서는 소멸자를 반드시 가상 소멸자로 선언하자

---
+++ 2. 생성자, 소멸자 및 대입 연산자/항목 7. 다형성을 가진 클래스에서는 소멸자를 반드시 가상 소멸자로 선언하자/example/example.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+class TimeKeeper {
+public:
+    TimeKeeper();
+    virtual ~TimeKeeper();
+};
+class AtomicClock : public TimeKeeper {};
+class WaterClock : public TimeKeeper {};
+class WristWatch: public TimeKeeper{};
+
+// TimeKeeper에서 파생된 클래스를 통해 동적으로 할당된 객체의 포인터를 반환
+TimeKeeper* getTimeKeeper();
+
+class Point {
+public:
+    Point(int xCoord, int yCoord);
+    ~Point();
+private:
+    int x, y;
+};
+
+// 가당찮은 아이디어입니다! std::string은 가상 소멸자가 없단 말입니다.
+class SpecialString : public std::string { };
+
+class AWOV {
+public:
+    virtual ~AWOV() = 0;
+};
+
+int main()
+{
+    // TimeKeeper 클래스 계통으로부터 동적으로 할당된 객체를 얻습니다.
+    TimeKeeper *ptk = getTimeKeeper();
+
+    // 자원 누출을 막기 위해 해제(삭제)합니다.
+    delete ptk;
+}
