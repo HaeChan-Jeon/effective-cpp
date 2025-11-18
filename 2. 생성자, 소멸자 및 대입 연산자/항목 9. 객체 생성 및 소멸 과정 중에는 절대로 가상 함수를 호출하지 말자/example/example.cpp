0672407a40f8aaa63aa0ad4b2b18b1a6aea85748
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-11-18 16:59:37 +0900

    STUDY: 항목 9 객체 생성 및 소멸 과정 중에는 절대로 가상 함수를 호출하지 말자 (1/2)

---
+++ 2. 생성자, 소멸자 및 대입 연산자/항목 9. 객체 생성 및 소멸 과정 중에는 절대로 가상 함수를 호출하지 말자/example/example.cpp
@@ -0,0 +1,34 @@
+﻿// 모든 거래에 대한 기본 클래스
+class Transaction {
+public:
+	Transaction();
+	// 타입에 따라 달라지는 로그 기록을 만듭니다.
+	virtual void logTransaction() const = 0;
+};
+
+// 기본 클래스 생성자의 구현
+Transaction::Transaction()
+{
+	// 마지막 동작으로, 이 거래를 로깅(하기 시작)합니다.
+	logTransaction();
+}
+
+// Transaction의 파생 클래스
+class BuyTransaction : public Transaction {
+public:
+	// 이 타입에 따른 거래내역 로깅을 구현합니다.
+	virtual void logTransaction() const;
+};
+
+// 역시 파생 클래스
+class sellTransaction : public Transaction {
+public:
+	// 이 타입에 따른 거래내역 로깅을 구현합니다.
+	virtual void logTransaction() const;
+};
+
+
+int main()
+{
+	BuyTransaction b;
+}
