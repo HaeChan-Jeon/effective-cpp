ac8ffbc1d1adc5bb7464a219bbfd2e1d32adcde9
JeonHaeChan <geojeonhc@gmail.com>
2025-11-18 13:57:48 +0900

    STUDY: 항목 6 컴파일러가 만들어낸 함수가 필요 없으면 확실히 이들의 사용을 금해 버리자

---
+++ 2. 생성자, 소멸자 및 대입 연산자/항목 6. 컴파일러가 만들어낸 함수가 필요 없으면 확실히 이들의 사용을 금해 버리자/example/example.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+//class HomeForSale {
+//public:
+//private:
+//    HomeForSale(const HomeForSale&);
+//    HomeForSale& operator=(const HomeForSale&);
+//};
+
+class HomeForSale : private Uncopyable {
+};
+
+class Uncopyable {
+protected:
+    Uncopyable() {}
+    ~Uncopyable() {}
+private:
+    Uncopyable(const Uncopyable&);
+    Uncopyable& operator=(const Uncopyable&);
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
