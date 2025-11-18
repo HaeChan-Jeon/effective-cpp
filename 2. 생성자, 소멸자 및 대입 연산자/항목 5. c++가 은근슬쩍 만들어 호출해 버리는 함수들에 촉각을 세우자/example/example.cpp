a3778a6243efa82672b2452181c9946289b3c2bd
JeonHaeChan <geojeonhc@gmail.com>
2025-11-18 13:49:02 +0900

    STUDY: 항목 5 c++가 은근슬쩍 만들어 호출해 버리는 함수들에 촉각을 세우자

---
+++ 2. 생성자, 소멸자 및 대입 연산자/항목 5. c++가 은근슬쩍 만들어 호출해 버리는 함수들에 촉각을 세우자/example/example.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+template<typename T>
+class NamedObject {
+public:
+    // 이 생성자는 이제 상수 타입의 name을 취하지 않습니다.
+    // nameValue가 비상수 string의 참조자가 되었기 때문입니다.
+    // 참조할 string을 가져야 하기 때문에 char*는 없애 버렸습니다.
+    NamedObject(const std::string& name, const T& value);
+private:
+    std::string& nameValue;
+    const T objectValue;
+};
+
+int main()
+{
+    std::string newDog("Persephone");
+    std::string oldDog("Satch");
+
+    NamedObject<int> p(newDog, 2);
+    NamedObject<int> s(oldDog, 36);
+
+    p = s;
+}
