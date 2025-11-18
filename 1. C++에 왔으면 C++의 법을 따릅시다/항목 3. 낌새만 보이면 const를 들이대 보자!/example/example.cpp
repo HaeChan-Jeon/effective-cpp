1f1e34218484493a8b86fb19080b109339790be2
JeonHaeChan <geojeonhc@gmail.com>
2025-11-18 10:58:45 +0900

    STUDY: 논리적 상수성

--- 1. C++에 왔으면 C++의 법을 따릅시다/항목 3. 낌새만 보이면 const를 들이대 보자!/example/example.cpp
+++ 1. C++에 왔으면 C++의 법을 따릅시다/항목 3. 낌새만 보이면 const를 들이대 보자!/example/example.cpp
@@ -2,22 +2,36 @@
 
 class CTextBlock {
 public:
-	// 부적절한 (그러나 비트수준 상수성이 있어서 허용되는) operator[]의 선언
-	char& operator[] (std::size_t position) const	
-	{ return pText[position]; }
+	std::size_t length() const;
 
 private:
 	char* pText;
+	// 바로 직전에 계산한 텍스트 길이
+	//std::size_t textLength;
+	// 이 길이가 현재 유효한가?
+	//bool lengthIsValid;
+
+	// 이 데이터 멤버들은 어떤 순간에도 수정이 가능합니다.
+	// 심지어 상수 멤버 함수 안에서도  수정할 수 있습니다.
+	mutable std::size_t textLength;
+	mutable bool lengthIsValid;
 };
+
+std::size_t CTextBlock::length() const
+{
+	if (!lengthIsValid) {
+		// 에러! 상수 멤버 함수 안에서는 textLength 및 lengthIsValid에 대입할 수 없습니다.
+		//textLength = std::strlen(pText);
+		//lengthIsValid = true;
+
+		// 이제 문제 없습니다.
+		textLength = std::strlen(pText);
+		lengthIsValid = true;
+	}
+
+	return textLength;
+}
 
 int main()
 {
-	// 상수 객체를 선언합니다.
-	const CTextBlock cctb("Hello");
-
-	// 상수 버전의 operator[]를 호출하여 cctb의 내부 데이터에 대한 포인터를 얻습니다.
-	char* pc = &cctb[0];
-
-	// cctb는 이제 "Jello라는 값을 갖습니다.
-	*pc = 'J';
 }
