3f5b24e83daa6b3dbdce0b8fb6b1449fd87b28e5
JeonHaeChan <geojeonhc@gmail.com>
2025-11-18 11:14:24 +0900

    STUDY: 상수 멤버 및 비상수 멤버 함수에서 코드 중복 현상을 피하는 방법

--- 1. C++에 왔으면 C++의 법을 따릅시다/항목 3. 낌새만 보이면 const를 들이대 보자!/example/example.cpp
+++ 1. C++에 왔으면 C++의 법을 따릅시다/항목 3. 낌새만 보이면 const를 들이대 보자!/example/example.cpp
@@ -1,36 +1,26 @@
 ﻿#include <iostream>
 
-class CTextBlock {
+class TextBlock {
 public:
-	std::size_t length() const;
-
-private:
-	char* pText;
-	// 바로 직전에 계산한 텍스트 길이
-	//std::size_t textLength;
-	// 이 길이가 현재 유효한가?
-	//bool lengthIsValid;
-
-	// 이 데이터 멤버들은 어떤 순간에도 수정이 가능합니다.
-	// 심지어 상수 멤버 함수 안에서도  수정할 수 있습니다.
-	mutable std::size_t textLength;
-	mutable bool lengthIsValid;
-};
-
-std::size_t CTextBlock::length() const
-{
-	if (!lengthIsValid) {
-		// 에러! 상수 멤버 함수 안에서는 textLength 및 lengthIsValid에 대입할 수 없습니다.
-		//textLength = std::strlen(pText);
-		//lengthIsValid = true;
-
-		// 이제 문제 없습니다.
-		textLength = std::strlen(pText);
-		lengthIsValid = true;
+	const char& operator[] (std::size_t position) const
+	{
+		return text[position];
 	}
 
-	return textLength;
-}
+	// 상수 버전 op[]를 호출하고 끝
+	char& operator[] (std::size_t position)
+	{
+		return
+			const_cast<char&>(					// op[]의 반환 타입에 캐스팅을 적용
+												// const를 떼어냅니다.
+				static_cast<const TextBlock&>	// *this의 타입에 const를 붙입니다.
+					(*this)[position]			// op[]의 상수 버전을 호출합니다.
+			);
+	}
+
+private:
+	char* text;
+};
 
 int main()
 {
